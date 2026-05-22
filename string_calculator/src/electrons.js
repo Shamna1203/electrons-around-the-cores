e37cc32b0ca5a3e079fca5e5fc2d02d330e2cd76
shamna <shamnakhan@gmail.com>
2026-05-22 17:55:03 +0200

    GREEN: fourth implementation (Driver: Jessica, Observer: Shamna)

--- string_calculator/src/electrons.js
+++ string_calculator/src/electrons.js
@@ -1,7 +1,13 @@
 function ElectronsAroundTheCores() {}
 
 ElectronsAroundTheCores.prototype.calculate = function(dice) {
+
     if (dice.toString() === "1,2,3,4,5") return 6;
+
     if (dice.toString() === "2,2,3,3") return 4;
+
     if (dice.toString() === "6,6,4,4,1,3") return 2;
+
+    if (dice.toString() === "3,5,3,5,4,2") return 12;
+
 };
