60bdc9d0f0f04f1c64d45ddefff55377c3cb849b
shamna <shamnakhan@gmail.com>
2026-05-22 17:35:18 +0200

    RED: first test (Driver: Jessica, Observer: Shamna)

--- string_calculator/spec/electrons_spec.js
+++ string_calculator/spec/electrons_spec.js
@@ -2,4 +2,8 @@ describe('Electrons Around The Cores', function() {
     beforeEach(function() {
         game = new ElectronsAroundTheCores();
     });
+
+    it('should return 6 for [1,2,3,4,5]', function() {
+        expect(game.calculate([1,2,3,4,5])).toEqual(6);
+    });
 });
